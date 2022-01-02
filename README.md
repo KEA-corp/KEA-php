80c62ab716d287ea02c8d13090a8ff825f4765b5
pf4-DEV <74263128+pf4-DEV@users.noreply.github.com>
2022-01-02 14:14:02 +0100

    push

---
+++ README.md
@@ -0,0 +1,14 @@
+# mode et args du kea brut
+
+| description             | mod | arg1          | arg2        | agr3        | arg4 |
+|-------------------------|-----|---------------|-------------|-------------|------|
+| affichier une variable  | A   | var           |             |             |      |
+| comparaison logique     | B   | var de sortie | var1        | comparateur | var2 |
+| calcul                  | C   | var de sortie | var1        | opération   | var2 |
+| debug                   | D   | on/off/print  |             |             |      |
+| sortie de boucle        | E   | nom de boucle |             |             |      |
+| boucle a tours          | L   | nom de boucle | nb de tours |             |      |
+| affichier du texte      | S   | texte         |             |             |      |
+| assignation de variable | V   | var           | valleur     |             |      |
+| execution si condition  | X   | nom de boucle | var (B)     |             |      |
+| break                   | Z   |               |             |             |      |
--- main.php
+++ main.php
@@ -14,6 +14,13 @@
 
 <?php
 
+function debug_print($texte){
+    global $DEBUG;
+    if ($DEBUG) {
+        echo $texte;
+    }
+}
+
 function getvar($name) {
     global $VARVAL, $VARNAME;
     for($i = 0; $i < count($VARNAME); $i++) {
@@ -31,17 +38,38 @@ function debugprint() {
     }
 }
 
+function compar( $comparateur, $var1, $var2) {
+    $a = getvar($var1);
+    $b = getvar($var2);
+    switch($comparateur) {
+        case "==":
+            return $a == $b;
+        case "!=":
+            return $a != $b;
+        case ">":
+            return $a > $b;
+        case "<":
+            return $a < $b;
+        case ">=":
+            return $a >= $b;
+        case "<=":
+            return $a <= $b;
+        default:
+            echo "Erreur de comparaison : " . $comparateur . "\n";
+    }
+}
+
 function setvar($name, $valeur, $ACTIVE) {
     global $VARVAL, $VARNAME;
     for($i = 0; $i < count($VARNAME); $i++) {
         if($VARNAME[$i] == $name) {
-            echo "$ACTIVE → modification: $name = $valeur\n";
+            debug_print("$ACTIVE → modification: $name = $valeur\n");
             $VARVAL[$i] = $valeur;
             return;
         }
     }
     
-    echo "$ACTIVE → ajout: $name = $valeur\n";
+    debug_print("$ACTIVE → ajout: $name = $valeur\n");
     array_push($VARNAME, $name);
     array_push($VARVAL, $valeur);
 }
@@ -71,7 +99,8 @@ function calc($calcul, $var1, $var2) {
 }
 
 function start ($code) {
-    global $VARVAL, $VARNAME;
+    global $VARVAL, $VARNAME, $DEBUG;
+    $DEBUG = false;
     $VARNAME = array();
     $VARVAL = array();
 
@@ -81,8 +110,20 @@ function start ($code) {
     codeinloop($code, "main" ,1);
 }
 
+function bcl_ctrl($code, $i, $nom, $nb){
+    $codetoloop = array();
+                    
+    for ($j = $i+1; $j < sizeof($code); $j++) {
+        array_push($codetoloop, $code[$j]);
+    }
+    
+    codeinloop($codetoloop, $nom, $nb);
+    return $nom;
+}
+
 function codeinloop($code, $nom ,$max) {
-    echo "demarrage de la boucle '$nom'\n";
+    global $DEBUG;
+    debug_print("demarrage de la boucle '$nom'\n");
     $sauter = "";
     for ($rep = 0; $rep < $max; $rep++) {
         for ($i = 0; $i < sizeof($code); $i++) {
@@ -92,7 +133,6 @@ function codeinloop($code, $nom ,$max) {
             $mode = $args[0];
             
             if($sauter == "" || ($mode == "E" && $args[1] == $sauter)){
-
                 $sauter = "";
 
                 if($mode == "") {
@@ -106,14 +146,7 @@ function codeinloop($code, $nom ,$max) {
                 }
 
                 else if ($mode == "L") {
-                    $codetoloop = array();
-                    
-                    for ($j = $i+1; $j < sizeof($code); $j++) {
-                        array_push($codetoloop, $code[$j]);
-                    }
-                    
-                    $sauter = $args[1];
-                    codeinloop($codetoloop, $args[1], $args[2]);
+                    $sauter = bcl_ctrl($code, $i, $args[1], $args[2]);
                 }
 
                 else if ($mode == "E") {
@@ -127,16 +160,50 @@ function codeinloop($code, $nom ,$max) {
                     setvar($args[1], $result, $nom);
                 }
 
-                else if ($mode == "B") {
+                else if ($mode == "Z") {
                     break;
-                }               
+                }
+
+                else if ($mode == "B") {
+                    setvar($args[1], compar($args[3], $args[2], $args[4]), $nom);
+                }
+                
+                else if ($mode == "D") {
+                    if ($args[1] == "on") {
+                        $DEBUG = true;
+                    }
+                    else if ($args[1] == "off") {
+                        $DEBUG = false;
+                    }
+                    else{
+                        debugprint();
+                    }
+                }
+
+                else if ($mode == "X") {
+                    if (getvar($args[2]) == true) {
+                        $sauter = bcl_ctrl($code, $i, $args[1], 1);
+                    }
+                    else {
+                        $sauter =  $args[1];
+                        debug_print("condition non remplie: $sauter\n");
+                    }
+                }
+
+                else if ($mode == "S") {
+                    echo $args[1] ."\n";
+                }
+
+                else if ($mode == "A") {
+                    echo getvar($args[1]);
+                }
 
                 else {
                     echo "Erreur: $mode\n";
                 }
             }
             else {
-                echo "$nom → Sauter: $ligne\n";
+                debug_print("$nom → sauter: $ligne\n");
             }
         }
     }
@@ -144,9 +211,22 @@ function codeinloop($code, $nom ,$max) {
 
 
 start("
-V i 0
+D off
+V a 0
+V 2 2
 V 1 1
-L bcl 10
-C i i + 1
-E bcl
+L find 100
+    C a a + 1
+    C mod a % 2
+    B pair mod == 0
+    B impair mod != 0
+    X Pprinter pair
+        A a
+        S _est_pair
+        E Pprinter
+    X Iprinter impair
+        A a
+        S _est_impair
+        E Iprinter
+    E find
 ");?>
